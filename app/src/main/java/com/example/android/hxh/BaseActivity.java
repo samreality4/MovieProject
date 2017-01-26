f7581b993085ee95fdbd56853fd9fec0f143c018
sam <swannsee20@gmail.com>
2017-01-25 20:34:37 -0800

    buttons update

--- app/src/main/java/com/example/android/hxh/BaseActivity.java
+++ app/src/main/java/com/example/android/hxh/BaseActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class BaseActivity extends AppCompatActivity {
 
